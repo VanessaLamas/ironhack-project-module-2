de4e52c2c091c13a9cc97ae23bf8e7eab429c6d7
Vanessa Lamas <120586605+VanessaLamas@users.noreply.github.com>
2023-04-30 18:52:29 +0200

    Add files via upload

---
+++ models/User.model.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    lastName:{
+      type: String,
+      required: true,
+      unique: false,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
